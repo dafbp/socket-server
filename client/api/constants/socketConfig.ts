40c594280caf16303e6931785a6788e399427045
doantuyen15 <64451486+doantuyen15@users.noreply.github.com>
2022-07-28 09:29:52 +0700

    change server local

--- client/api/constants/socketConfig.ts
+++ client/api/constants/socketConfig.ts
@@ -1,8 +1,8 @@
 
 
 
-// export const SOCKET_SERVER = 'http://localhost:3005'
-export const SOCKET_SERVER = 'http://ec2-18-136-105-202.ap-southeast-1.compute.amazonaws.com:3005'
+export const SOCKET_SERVER = 'http://localhost:3005'
+// export const SOCKET_SERVER = 'http://ec2-18-136-105-202.ap-southeast-1.compute.amazonaws.com:3005'
 
 export const REQ_CHANELS = {
     method: "method"
