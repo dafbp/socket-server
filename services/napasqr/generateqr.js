5c7c81b141cfa0f32e0f432f2d592d61bc93bc76
nvd <hos6@altisss.vn>
2022-08-22 23:16:59 +0700

    Generate ngan hang ok

--- services/napasqr/generateqr.js
+++ services/napasqr/generateqr.js
@@ -6,7 +6,7 @@ const generateQrInitString = ({ acount_no, amount, note, bank_code }) => {
     const infomatrix = [
         ['00', '01'], // Payload Format Indicator/ Phiên bản dữ liệu
         ['01', '12'], // Point of Initiation Method/ Phương thức khởi tạo
-        ['38', `0010A00000072701270006${bank_code}60113${acount_no}0208QRIBFTTA`], // Consumer Account Information/ Thông tin định danh người thụ hưởng
+        ['38', `0010A00000072701270006${bank_code}0113${acount_no}0208QRIBFTTA`], // Consumer Account Information/ Thông tin định danh người thụ hưởng
         ['53', '704'], // Transaction Currency/ Mã tiền tệ
         ['54', amount], // Transaction Amount/ Số tiền GD
         ['58', 'VN'], // Country Code/ Mã quốc gia
